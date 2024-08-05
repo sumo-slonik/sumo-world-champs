c88a0e321a8b1b9c5375d9b46387c9cebacee7cc
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-08-05 22:52:10 +0200

    added special map component with special movement limitations for easier navigation of the visitors

--- world_sumo_championships/app/(tabs)/competitionInfo.tsx
+++ world_sumo_championships/app/(tabs)/competitionInfo.tsx
@@ -1,11 +1,15 @@
 import {Text, View} from "react-native";
 import * as React from "react";
 import {styles} from "@/components/custom_components/myStyles";
+import {MyMap} from "@/components/custom_components/mapComponent";
 
+//klucz API AIzaSyC8P1BEj2XPEyOB_vEaNCpxmlmEn91vdMs
 export const CompetitionInfoScreen =() => {
     return (
         <View style={[styles.defaultBackground]}>
             <Text> competitionInfo</Text>
+            <MyMap></MyMap>
+
         </View>
     );
 };
